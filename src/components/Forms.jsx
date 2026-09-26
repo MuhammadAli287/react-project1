b09038da08871215d0b21f2799dfdaf03d8e9b89
Ali <i.muhammad.ali.287@gmail.com>
2026-09-26 19:10:22 +0500

    apply border in form

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -3,8 +3,8 @@
 function Forms(){
     return(
         
-<div className="bg-gray-100/80 p-8 ">
-<form class="max-w-md mx-auto bg-white rounded-lg  p-10 shadow-2xl ">
+<div className="bg-gray-100/80 p-8  ">
+<form class="max-w-md mx-auto bg-white rounded-lg  p-10 shadow-2xl border-1 border-gray-300 ">
   <div class="relative z-0 w-full mb-5 group">
       <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer" placeholder=" " required />
       <label for="floating_email" class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Email address</label>
